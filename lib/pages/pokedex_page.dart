20d7cad477946f4e637afafd5b53341921fa293c
Wilson Lluilema <wilsonlluilema007@gmail.com>
2022-03-19 05:20:08 -0500

    terminado de cargar los elementos

--- lib/pages/pokedex_page.dart
+++ lib/pages/pokedex_page.dart
@@ -15,6 +15,7 @@ class _PokedexPageState extends State<PokedexPage> {
   var pokeApi =
       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
   List? pokedexkanto;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -27,131 +28,239 @@ class _PokedexPageState extends State<PokedexPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pokedex'),
-        backgroundColor: Colors.deepPurpleAccent[100],
-      ),
-      body: Stack(
-        children: [
-          const BackgroundW(),
-          ListView.builder(
+        body: Stack(
+      children: [
+        const BackgroundW(),
+        ListView.builder(
             itemCount: pokedexkanto?.length,
-            itemBuilder: (context, index) => Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(62, 66, 107, 0.9),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-                  height: 320,
-                  width: double.infinity,
-                  child: Container(
-                    margin: const EdgeInsets.only(left: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              height: 35,
-                              width: 35,
-                              decoration: BoxDecoration(
-                                  color: Colors.green,
-                                  borderRadius: BorderRadius.circular(15)),
-                              margin: const EdgeInsets.only(top: 20, right: 5),
-                              child: const Icon(
-                                Icons.person_rounded,
-                                color: Colors.white,
+            itemBuilder: (context, index) {
+              var tipo = pokedexkanto?[index]['type'][0];
+              return Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: tipo == 'Grass'
+                          ? const Color.fromRGBO(43, 209, 171, 1)
+                          : tipo == 'Fire'
+                              ? Colors.redAccent
+                              : tipo == 'Water'
+                                  ? Colors.blue
+                                  : tipo == 'Electric'
+                                      ? Color.fromARGB(255, 230, 209, 20)
+                                      : tipo == 'Rock'
+                                          ? Colors.grey
+                                          : tipo == 'Ground'
+                                              ? Colors.brown
+                                              : tipo == 'Psychic'
+                                                  ? Colors.indigo
+                                                  : tipo == 'Fighting'
+                                                      ? Colors.orange
+                                                      : tipo == 'Bug'
+                                                          ? const Color
+                                                                  .fromARGB(
+                                                              255, 9, 199, 57)
+                                                          : tipo == 'Ghost'
+                                                              ? Colors
+                                                                  .deepPurple
+                                                              : tipo == 'Normal'
+                                                                  ? const Color
+                                                                          .fromRGBO(
+                                                                      233,
+                                                                      100,
+                                                                      100,
+                                                                      1)
+                                                                  : tipo ==
+                                                                          'Posion'
+                                                                      ? Colors
+                                                                          .purple
+                                                                      : Colors
+                                                                          .purple,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    margin: const EdgeInsets.only(right: 30, top: 50, left: 30),
+                    height: 320,
+                    width: double.infinity,
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(top: 15, left: 15),
+                            child: Row(
+                              children: [
+                                const Icon(
+                                  Icons.personal_video,
+                                  color: Colors.white,
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Text(
+                                  '#${pokedexkanto?[index]['num']}',
+                                  style: const TextStyle(
+                                      fontSize: 25, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 70,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                pokedexkanto?[index]['name'] ?? 'N/D',
+                                maxLines: 2,
+                                style: const TextStyle(
+                                  fontSize: 29,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
                               ),
-                            ),
-                            Text(
-                              '${pokedexkanto?[index]['num']}' ?? 'N/D',
-                              style:
-                                  TextStyle(fontSize: 20, color: Colors.white),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 50,
-                        ),
-                        Row(
-                          children: const [
-                            Text(
-                              'Habil',
-                              style:
-                                  TextStyle(fontSize: 25, color: Colors.white),
-                            ),
-                            Icon(
-                              Icons.person,
-                              color: Colors.blue,
-                              size: 40,
-                            )
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 25,
-                        ),
-                        Text(
-                          pokedexkanto![index]['name'] ?? 'N/D',
-                          style: const TextStyle(
-                            fontSize: 29,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+                              const Icon(
+                                Icons.star,
+                                color: Color.fromARGB(255, 241, 201, 56),
+                                size: 40,
+                              )
+                            ],
                           ),
-                        ),
-                        const Text(
-                          'PoderdeHabilidad',
-                          style: TextStyle(
-                            fontSize: 25,
-                            color: Colors.white,
+                          const SizedBox(
+                            height: 25,
                           ),
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                              primary: Colors.blue,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 30, vertical: 15),
+                          Text(
+                            'Height : ${pokedexkanto?[index]['height']} ',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            'Weight : ${pokedexkanto?[index]['weight']} ',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          SizedBox(
+                            height: 50.0,
+                            child: RaisedButton(
+                              onPressed: () {},
                               shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(25))),
-                          onPressed: () {},
-                          child: const Text(
-                            'Ver mas',
-                            style: TextStyle(fontSize: 20),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 200,
-                  bottom: 100,
-                  child: FadeInImage(
-                    height: 300,
-                    width: 300,
-                    placeholder: const AssetImage(
-                      'assets/img/pokemon1.png',
-                    ),
-                    image: NetworkImage(
-                        pokedexkanto?[index] != null
-                            ? pokedexkanto![index]['img']
-                            : const AssetImage(
-                                'assets/img/pokemon1.png',
+                                  borderRadius: BorderRadius.circular(80.0)),
+                              padding: const EdgeInsets.all(0.0),
+                              child: Ink(
+                                decoration: BoxDecoration(
+                                    gradient: const LinearGradient(
+                                      colors: [
+                                        Color.fromARGB(255, 91, 58, 240),
+                                        Color.fromARGB(255, 58, 39, 141)
+                                      ],
+                                      begin: Alignment.centerLeft,
+                                      end: Alignment.centerRight,
+                                    ),
+                                    borderRadius: BorderRadius.circular(30.0)),
+                                child: Container(
+                                  constraints: const BoxConstraints(
+                                      maxWidth: 100.0, minHeight: 50.0),
+                                  alignment: Alignment.center,
+                                  child: Text(
+                                    '${pokedexkanto?[index]['type'][0]}',
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
                               ),
-                        scale: 0.5),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+                  Positioned(
+                    top: 90,
+                    left: 270,
+                    child: Container(
+                      height: 150,
+                      width: 150,
+                      decoration: BoxDecoration(
+                          color: tipo == 'Grass'
+                              ? const Color.fromRGBO(105, 240, 174, 0.9)
+                              : tipo == 'Fire'
+                                  ? const Color.fromARGB(255, 241, 48, 48)
+                                  : tipo == 'Water'
+                                      ? const Color.fromARGB(255, 23, 113, 187)
+                                      : tipo == 'Electric'
+                                          ? const Color.fromARGB(
+                                              255, 156, 142, 12)
+                                          : tipo == 'Rock'
+                                              ? const Color.fromARGB(
+                                                  255, 85, 80, 80)
+                                              : tipo == 'Ground'
+                                                  ? const Color.fromARGB(
+                                                      255, 77, 50, 40)
+                                                  : tipo == 'Psychic'
+                                                      ? const Color.fromARGB(
+                                                          255, 35, 56, 175)
+                                                      : tipo == 'Fighting'
+                                                          ? const Color
+                                                                  .fromARGB(
+                                                              255, 248, 134, 3)
+                                                          : tipo == 'Bug'
+                                                              ? Colors
+                                                                  .lightGreenAccent
+                                                              : tipo == 'Ghost'
+                                                                  ? Colors
+                                                                      .deepPurple
+                                                                  : tipo ==
+                                                                          'Normal'
+                                                                      ? Colors
+                                                                          .black26
+                                                                      : tipo ==
+                                                                              'Posion'
+                                                                          ? const Color.fromARGB(
+                                                                              255,
+                                                                              209,
+                                                                              17,
+                                                                              243)
+                                                                          : const Color.fromARGB(
+                                                                              255,
+                                                                              137,
+                                                                              19,
+                                                                              158),
+                          borderRadius: BorderRadius.circular(100)),
+                    ),
+                  ),
+                  Positioned(
+                    left: 200,
+                    bottom: 100,
+                    child: FadeInImage(
+                      height: 300,
+                      width: 300,
+                      placeholder: const AssetImage(
+                        'assets/img/ashtrans.png',
+                      ),
+                      image: NetworkImage(
+                          pokedexkanto?[index] != null
+                              ? pokedexkanto![index]['img']
+                              : const AssetImage(
+                                  'assets/img/pokemon1.png',
+                                ),
+                          scale: 0.5),
+                    ),
+                  )
+                ],
+              );
+            })
+      ],
+    ));
   }
 
   void fetchPokemonData() async {
